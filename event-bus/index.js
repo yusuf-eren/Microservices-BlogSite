b6d8d9ae7f6f92c8333163138bb13d286068160a
Yusuf Eren <erenyusuf170@gmail.com>
2022-08-20 16:29:21 +0300

    Query

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,7 +9,7 @@ app.post("/events", (req, res) => {
   const event = req.body;
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
-  //axios.post("http://localhost:4002/events", event);
+  axios.post("http://localhost:4002/events", event);
 
   res.send({ status: "OK" });
 });
