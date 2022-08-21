def1081385bee954e21fb383f838ac7ef78d34e9
Yusuf Eren <erenyusuf170@gmail.com>
2022-08-21 14:15:04 +0300

    MICROSERVICES

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,8 +7,8 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
-
+    const res = await axios.get("http://localhost:4002/posts");
+    
     setPosts(res.data);
   };
 
@@ -25,7 +25,7 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
