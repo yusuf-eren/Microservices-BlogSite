c5d898b13ac81fb51976c525c437333b6c087a0b
Yusuf Eren <erenyusuf170@gmail.com>
2022-08-24 18:06:32 +0300

    Microservice k8

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,18 +10,18 @@ const events = [];
 app.post("/events", (req, res) => {
   const event = req.body;
   events.push(event)
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
+  // axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
 
   res.send({ status: "OK" });
 });
--- query/index.js
+++ query/index.js
@@ -53,7 +53,7 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log("http://localhost:4002 Query");
   try {
-    const res = await axios.get("http://localhost:4005/events");
+    const res = await axios.get("http://event-bus-srv:4005/events");
 
     for (let event of res.data) {
       console.log("Processing event:", event.type);
